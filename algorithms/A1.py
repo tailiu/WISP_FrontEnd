e6bdec34a575855c44af10f7e3bce054ab896857
Thomas Poetsch <thp@comnets.uni-bremen.de>
2017-03-05 16:43:31 +0400

    changed dummy network algorithm to read json file

--- algorithms/A1.py
+++ algorithms/A1.py
@@ -1,57 +1,9 @@
 #!/usr/bin/python
-
 import sys
 import json
-import time
+from pprint import pprint
 
-# Output format
-output = {
-	'bandwidth': 9787987987, 
-	'costs': 12342,
-	'nodes': [
-		{
-			'node': 7899,
-			'nodeProperty': {
-				'capacity': 8908098,
-				'mountingHeight': 789787,
-				'type': 'source'
-			},
-		},
-		{
-			'node': 30010,
-			'nodeProperty': { 
-				'capacity': 8908098,
-				'mountingHeight': 789787,
-				'type': 'intermediate'
-			}
-		},
-		{
-			'node': 20090,
-			'nodeProperty': {
-				'capacity': 123123123,
-				'mountingHeight': 546456546,
-				'type': 'sink'
-			}
-		}
-	],
-	'edges': [
-		{
-			'nodes': [7899, 30010],
-			'edgeProperty': {
-				'length': 1231213,
-				'bandwidth': 88098098,
-				'frequency': 1321312312
-			}
-		},
-		{
-			'nodes': [30010, 20090],
-			'edgeProperty': {
-				'length': 6546546456,
-				'bandwidth': 7677777,
-				'frequency': 1231231233123
-			}
-		}         
-	]
-}
+with open('/home/thp/WISPFrontEnd/algorithms/data_test.json') as data_file:
+    data = json.load(data_file)
 
-print json.dumps(output)
+print json.dumps(data)
